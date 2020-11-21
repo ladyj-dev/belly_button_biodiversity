481bac69052a736c2ea9e254632f72c4a6510956
Arica Jackson <aricajackson2020@gmail.com>
2020-11-21 15:05:46 -0500

    suedocoding

---
+++ resources/StarterCode/static/js/app.js
@@ -0,0 +1,61 @@
+// 
+
+
+// create a function called buildBiodata
+function buildBiodata(sample) {
+    // grab data using d3.json and use then method to chain
+
+
+
+    // filter data
+
+    // grab panel from index html panel class (can be seen inside inspect) by grabing sample metadata id
+    var Panel = d3.select("#sample-metadata");
+    // clear html in the panel 
+    Panel.html("");
+    
+    // Object.entries
+}
+
+
+
+
+
+
+// create an init function
+function init() {
+    // get a reference to selDataset
+
+
+
+    // create a dropdown menu with names
+
+
+
+    // call functions;
+    
+    // grab initial value - sample #947 (testSample)
+    
+    
+    // buildBiodata(testSample)
+
+
+    // buildCharts(testSample)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// create the init function
+init();
